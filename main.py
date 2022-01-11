a5ab15e4be91fbe3deec083897fe9bf118b6d9a2
Luna-Karch <96808999+Luna-Karch@users.noreply.github.com>
2022-01-11 16:40:22 -0500

    help command + invite command

--- main.py
+++ main.py
@@ -101,7 +101,52 @@ async def dm_user(
         warn("what are you sending this user?")
 
 
+# HELP COMMAND
+
+
+@client.command(name="help")
+async def help_command(ctx, _type: str = None):
+    em = discord.Embed(title="Categories", color=0xFF0000)
+    em.set_footer(
+        text="type `.help [category] for help with a specific category or command`"
+    )
+    if not _type:
+        em.set_author(name="Help")
+        categories = "**Currency**\n**Information**"
+        em.description = categories
+    elif _type.lower() == "currency":
+        em.title = "Currency Commands"
+        currency_commands = [
+            "**Balance**",
+            "**Rob**",
+            "**Work**",
+            "**Deposit**",
+            "**Withdraw**",
+            "**Give**",
+        ]
+        currency_commands = ", ".join(currency_commands)
+        em.description = currency_commands
+    elif _type.lower() in ("information", "infoh"):
+        information_commands = ["**Invite**", "**Info**"]
+        information_commands = ", ".join(information_commands)
+        em.description = information_commands
+    elif _type.lower() in ("bal", "balance"):
+        em.description = "Tells you your balance or someone elses\nOne optional argument: [user ping or id]\nUsage: `.bal [user ping or id]`"
+    await ctx.send(embed=em)
+
+
 # random commands
+@client.command(name="invite", aliases=["inv"])
+async def invite_bot(ctx):
+    em = discord.Embed(
+        title="Click here to invite!",
+        url="https://discord.com/api/oauth2/authorize?client_id=924293465997705286&permissions=275884665968&scope=bot",
+    )
+    em.color = 0xFF0000
+    em.set_author(name=client.user, icon_url=client.user.avatar_url)
+    await ctx.send(embed=em)
+
+
 @client.command(name="info")
 async def info(ctx: commands.Context, _id: str = None):
     if _id == None:
