5181ce112f9230cbe5793519d4ac5a0e4f186af3
Luna-Karch <96808999+Luna-Karch@users.noreply.github.com>
2022-01-15 14:27:20 -0500

    Merge branch 'master' of https://github.com/ddjerqq/Anarchist

--- main.py
+++ main.py
@@ -19,52 +19,12 @@ PREFIX = "."
 client = commands.Bot(command_prefix=PREFIX, intents=discord.Intents.all())
 client.remove_command("help")
 # ~~~~~~~~~~~~~~~~~~~~~~~
-
-@tasks.loop(seconds = 120)
-async def save_database() -> None:
-    database._save()
-    database.generate_csv()
-
-@client.event
-async def on_ready():
-    old_user_count = len(database)
-    for guild in client.guilds:
-        async for member in guild.fetch_members(limit=None):
-            if member.bot:
-                continue
-            if member.id not in database:
-                database.add_user(member.id, member.name)
-            else:
-                tmp_user = database[member.id]
-                if tmp_user["name"] != member.name:
-                    old_name = tmp_user["name"]
-                    tmp_user["name"] = member.name
-                    database[member.id] = tmp_user
-                    warn(f"{old_name}'s name updated to {tmp_user['name']}")
-
-    warn(f"added {len(database) - old_user_count} new users")
-
-    await client.change_presence(activity=discord.Game(F"{PREFIX}help"))
-    ok("Bot is online")
-
-@client.event
-async def on_command_error(ctx, error):
-    embed = discord.Embed(color=0xFF0000, title="unhandled error")
-    embed.add_field(name="error:", value=error, inline=False)
-    await ctx.send(embed=embed)
-    warn(error)
-
-@client.event
-async def on_member_join(member):
-    if member.bot:
-        return
-    if member.id not in database:
-        database.add_user(member.id, member.name)
-
 # utils
-async def dm_user(_id: str, *, message: str = None, embed: discord.Embed = None) -> None:
+async def dm_user(
+    _id: str, *, message: str = None, embed: discord.Embed = None
+) -> None:
     """
-        dm an user by their id
+    dm an user by their id
     """
     user = client.get_user(_id)
     if message == None:
@@ -74,150 +34,28 @@ async def dm_user(_id: str, *, message: str = None, embed: discord.Embed = None)
     else:
         warn("what are you sending this user?")
 
-@client.command(name = "help")
-async def help_command(ctx, _type: str = None):
-    em = discord.Embed(title="Categories", color=0xFF0000)
-    em.set_footer(
-        text="type `.help [category] for help with a specific category or command`"
-    )
-    if not _type:
-        em.set_author(name="Help")
-        categories = "**Currency**\n**Information**"
-        em.description = categories
-    elif _type.lower() == "currency":
-        em.title = "Currency Commands"
-        currency_commands = [
-            "**Balance**",
-            "**Rob**",
-            "**Work**",
-            "**Deposit**",
-            "**Withdraw**",
-            "**Give**",
-        ]
-        currency_commands = ", ".join(currency_commands)
-        em.description = currency_commands
-    await ctx.send(embed=em)
 
-@client.command(name = "info")
-async def info(ctx: commands.Context, user: discord.Member = None):
-    if not user: 
-        user = ctx.author
+# TODO add query commands for mods
 
-    embed: discord.Embed = discord.Embed( 
-        title = f"{user.name}#{user.discriminator}'s info", 
-        color = 0xff0000
-        )
-    embed.add_field( 
-        name   = "creation time",
-        value  = str(user.created_at),
-        inline = False
-        )
-    embed.add_field( 
-        name   = "id",
-        value  = user.id,
-        inline = False
-        )
-    embed.add_field( 
-        name   = "is bot?",
-        value  = user.bot,
-        inline = False
-        )
-    embed.set_image(
-        url = user.avatar_url
-    )
 
-    await ctx.send(embed=embed)
+@tasks.loop(seconds=120)
+async def save_database() -> None:
+    database._save()
+    database.generate_csv()
 
-#TODO add query commands for mods
-
-@client.command(name = "rank", aliases = ["leaderboard"])
-async def _rank(ctx: commands.Context) -> None:
-    users = []
-    async for user in ctx.guild.fetch_members( limit = None ):
-        if not user.bot:
-            users.append(database[user.id])
-
-    sorted_users = sorted(users, key = lambda x: x["amount"], reverse = True)[0:10]
-
-    embed = discord.Embed(
-        title = f"{ctx.guild.name} leaderboard",
-        color = 0xff0000,
-    )
-    for user_index in range(len(sorted_users)):
-        embed.add_field(
-            name   = f"#{user_index + 1} {sorted_users[user_index]['name']}",
-            value  = str(sorted_users[user_index]["amount"]) + " ⏣",
-            inline = False 
-        )
-    await ctx.send( embed = embed)
-
-@client.command(name = "bal", aliases = ["balanace"])
-async def balance(ctx: commands.Context, user: discord.Member = None) -> None:
-    if not user: user = ctx.author
-    _id = user.id
-    
-    embed = discord.Embed( 
-        color = 0xff0000, 
-        title = f"{database[_id]['name']}'s balance" 
-        )
-    embed.add_field(
-        name = "total amount",
-        value = f"{database[_id]['amount']} ⏣",
-        inline = False 
-        )
-    await ctx.send( embed = embed )
-
-@client.command(name = "work")
-@commands.cooldown(1, 30, commands.BucketType.user)
-async def work(ctx: commands.Context) -> None:
-    database.work(ctx.author.id)
-    embed = discord.Embed( 
-        color = 0xff0000, 
-        title = f"nice work! \nyou earned 25 ⏣"
-        )
-    await ctx.send( embed = embed )
-
-@client.command(name = "give")
-async def give(ctx: commands.Context, user: discord.Member, amount: str) -> None:
-    _id = user.id
-    if amount.lower() == "all" or amount.lower() == "max":
-        amount = database[ctx.author.id]["amount"]
-    else:
-        if int(amount) > 0:
-            amount = int(amount)
-        else:
-            embed = discord.Embed( 
-                color = 0xff0000, 
-                title = f"what are you trying to do here 🤨⁉"
-            )
-            await ctx.send(embed = embed)
-            return
-
-    if database.give(ctx.author.id, _id, amount):
-        embed = discord.Embed( 
-            color = 0xff0000, 
-            title = f"you successfully gave {database[_id]['name']} {amount} ⏣"
-            )
-        # embed2 = discord.Embed(
-        #     color = 0xff0000, 
-        #     title = f"{database[ctx.author.id]['name']} gave you {amount} coins"
-        #     )
-        # await dm_user(_id, embed = embed2)
-    else:
-        embed = discord.Embed( 
-            color = 0xff0000, 
-            title = f"you don't have enough money dumbass"
-            )
-
-    await ctx.send( embed = embed )
 
 def main():
     save_database.start()
     client.run(TOKEN)
 
+
 if __name__ == "__main__":
     try:
+        for cog in os.listdir("cogs"):
+            client.load_extension(f"cogs.{cog[:-3]}")
+            print(f"{cog[:-3]} Loaded.")
         main()
+
     except KeyboardInterrupt:
         warn("keyboard interrupt")
     except Exception as e:
--- utils.py
+++ utils.py
@@ -9,9 +9,9 @@ END = "\33[0m"
 
 def stat(precision: int = 6) -> callable:
     """
-        decorator to measure the time a function takes
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        precision (int, optional): floating point numbers of the precision. Defaults to 4.
+    decorator to measure the time a function takes
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    precision (int, optional): floating point numbers of the precision. Defaults to 4.
     """
 
     def real_stat(func) -> callable:
@@ -34,68 +34,76 @@ def stat(precision: int = 6) -> callable:
             )
 
             return return_val
+
         return wrap
+
     return real_stat
+
 
 def warn(message="Warning!") -> None:
     """
-        [^] use this to print warnings.\n
-        yellow
+    [^] use this to print warnings.\n
+    yellow
     """
     os.system("")
     print("\033[38;2;255;125;0m" + f"[^] {str(message).strip()}" + "\a" + END)
 
+
 def ok(message="Success") -> None:
     """
-        [*] use this to print sucess messages.\n
-        green
+    [*] use this to print sucess messages.\n
+    green
     """
     os.system("")
     print("\033[38;2;0;255;0m" + f"[*] {str(message).strip()}" + "\a" + END)
 
+
 def rgb(text: str, /, color: str | tuple | int, *, newline: bool = True) -> None:
     """
-        print rgb color 🎊 with this
-        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        \n
-        ~~~~~~~~~~~~~~
-        Args:
-            text  (str): the text you want to print, str() method is automatically called on it
-            color (str): #000000 hex representation of color, prefixed with # or not
-            color (tuple): (red, green, blue) color tuple
-            color (int): 0xff0000 integer representation of hex color.
-            newline (bool default False): whether or now you want to print a new line \n
-            after you are done printing rgb, you can insert colored text if you set this to false
-        \n
-        ~~~~~~~~~~~~~~
-        Retrurns:
-            None
-        \n
-        ~~~~~~~~~~~~~~
-        Example:
-            >>> rgb("lorem ipsum", "#ff0000")
-            >>> rgb("lorem ipsum", (255, 0, 0))
-            >>> rgb("lorem ipsum", 0xff0000)
-            >>> rgb("lorem", "#ff0000", newline=False)
-            >>> rgb("ipsum", "#00ff00", newline=False)
+    print rgb color 🎊 with this
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    \n
+    ~~~~~~~~~~~~~~
+    Args:
+        text  (str): the text you want to print, str() method is automatically called on it
+        color (str): #000000 hex representation of color, prefixed with # or not
+        color (tuple): (red, green, blue) color tuple
+        color (int): 0xff0000 integer representation of hex color.
+        newline (bool default False): whether or now you want to print a new line \n
+        after you are done printing rgb, you can insert colored text if you set this to false
+    \n
+    ~~~~~~~~~~~~~~
+    Retrurns:
+        None
+    \n
+    ~~~~~~~~~~~~~~
+    Example:
+        >>> rgb("lorem ipsum", "#ff0000")
+        >>> rgb("lorem ipsum", (255, 0, 0))
+        >>> rgb("lorem ipsum", 0xff0000)
+        >>> rgb("lorem", "#ff0000", newline=False)
+        >>> rgb("ipsum", "#00ff00", newline=False)
     """
     if type(color) == str:
         color = tuple(int(color.lstrip("#")[i : i + 2], 16) for i in (0, 2, 4))
-    
-    elif type(color) == tuple: pass
+
+    elif type(color) == tuple:
+        pass
 
     elif type(color) == int:
-        blue  = color % 256
-        green = ( (color - blue) // 256 ) % 256
-        red   = ( (color - blue) // 256 ** 2 ) - green // 256
-        color = ( (red, green, blue) )
-    
-    else: raise Exception(f"invalid color {color}")
+        blue = color % 256
+        green = ((color - blue) // 256) % 256
+        red = ((color - blue) // 256 ** 2) - green // 256
+        color = (red, green, blue)
 
-    if sum(color) > 765: return
+    else:
+        raise Exception(f"invalid color {color}")
+
+    if sum(color) > 765:
+        return
 
     end = "\n" if newline else ""
     _color = f"\033[38;2;{color[0]};{color[1]};{color[2]}m"
     _end_char = "\033[0m"
 
-    print( _color + str(text) + _end_char, end=end )
+    print(_color + str(text) + _end_char, end=end)
